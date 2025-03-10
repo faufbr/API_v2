c0c3ffbba7a2b22782a39a0146f0b1fd16fdb4a2
faufbr <faustine.fabre44@gmail.com>
2025-03-10 10:27:40 +0100

    last week database

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 python -m pip install --upgrade pip wheel
 python -m pip install httpie
 
+# Install php DI
+composer require php-di/php-di
 
 # Run app
 php -S localhost:8080 C:\wamp64\www\API_v2\public\index.php
--- public/index.php
+++ public/index.php
@@ -5,15 +5,27 @@ declare(strict_types=1);
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use DI\Container;
 
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+$container = new Container;
+
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
 
-    $body = json_encode([1 => "One"]);
+
+    $database = $this->get(App\Database::class);
+
+    $repository = new App\Repositories\SoinsRepository($database);
+
+    $data = $repository->getAll();
+
+    $body = json_encode($data);
     
     $response->getBody()->write($body);
 
